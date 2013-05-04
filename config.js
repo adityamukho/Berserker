f9c3ec67f303642345e2b094ac57063444d2a80b
Aditya Mukhopadhyay <aditya.mukhopadhyay@gmail.com>
2013-05-04 17:06:28 +0530

    Applied Apache v2 Licence

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 var config = {
     aria2c_opts: {
-        "rpc-listen-port": 6800,
-        "dir": "/home/aditya/Downloads/aria2",
-        "log": "/home/aditya/.aria2/aria2.log",
-        "log-level": "error",
-        "dht-listen-port": 6801,
-        "rpc-user": "aditya",
-        "rpc-passwd": "booger",
-        "min-split-size": "1M",
-        "max-connection-per-server": 4,
-        "save-session": "/home/aditya/.aria2/session",
-        "input-file": "/home/aditya/.aria2/session"
+        'rpc-listen-port': 6800,
+        'dir': '/home/aditya/Downloads/aria2',
+        'log': '/home/aditya/.aria2/aria2.log',
+        'log-level': 'error',
+        'dht-listen-port': 6801,
+        'rpc-user': 'aditya',
+        'rpc-passwd': 'booger',
+        'min-split-size': '1M',
+        'max-connection-per-server': 4,
+        'save-session': '/home/aditya/.aria2/session',
+        'input-file': '/home/aditya/.aria2/session'
     },
     berserker_opts: {
         aria2c_executable: 'aria2c'
