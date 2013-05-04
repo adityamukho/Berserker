a7352413d9b6f47f268613f19b88cce1d1f7d00b
Aditya Mukhopadhyay <aditya.mukhopadhyay@gmail.com>
2013-05-04 16:49:56 +0530

    Launches and connects to aria2c instance.
    Known Issues:
    ~~~~~~~~~~~~~
    1. Child process not getting detached for some reason.

--- Berserker.js
+++ Berserker.js
@@ -1,15 +1,35 @@
-var config = require('./config');
-var cp = require('child_process');
+var config = require('./config'),
+        cp = require('child_process'),
+        restify = require('restify'),
+        assert = require('assert'),
+        fs = require('fs');
 
 var args = [
-  '--enable-rpc=true',
-  '--rpc-listen-all=true',
-  '-D'
+    '--enable-rpc=true',
+    '--rpc-listen-all=true',
+    '-D'
 ];
-for (var key in config) {
-  args.push('--' + key + '=' + config[key]);
+for (var key in config.aria2c_opts) {
+    args.push('--' + key + '=' + config.aria2c_opts[key]);
 }
 
 console.log('Starting Aria 2 in daemon mode...');
-var aria2c = cp.spawn('aria2c', args, { detached: true, stdio: 'ignore' });
+var aria2c = cp.spawn(config.berserker_opts.aria2c_executable, args, {detached: true, stdio: 'ignore'});
 aria2c.unref();
+
+var client = restify.createJsonClient({
+    url: 'http://localhost:' + config.aria2c_opts['rpc-listen-port'],
+    version: '*'
+});
+client.basicAuth(config.aria2c_opts['rpc-user'], config.aria2c_opts['rpc-passwd']);
+
+var goptions = {
+    jsonrpc: '2.0',
+    id: 'berserker',
+    method: 'aria2.getGlobalOption'
+};
+client.post('/jsonrpc', goptions, function(err, req, res, obj) {
+  assert.ifError(err);
+  console.log('%d -> %j', res.statusCode, res.headers);
+  console.log('%j', obj);
+});
