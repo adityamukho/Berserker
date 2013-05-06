7d863bfa379d4cd0f4c76b5b3ad97b907e9665f7
Aditya Mukhopadhyay <aditya.mukhopadhyay@gmail.com>
2013-05-06 20:51:34 +0530

    Serves NG pages properly. REST commands impl in progress.

--- node/Berserker.js
+++ node/Berserker.js
@@ -1,9 +1,7 @@
 //Required modules
 var config = require('./config'),
         conn = require(__dirname + '/connectors/' + config.berserker_opts.connector),
-        cp = require('child_process'),
-        restify = require('restify'),
-        socketio = require('socket.io');
+        cp = require('child_process');
 
 //Argument array for the aria2c process
 var args = ['--enable-rpc=true', '--rpc-listen-all=true', '-D'];
@@ -11,7 +9,7 @@ for (var key in config.aria2c_opts) {
     args.push('--' + key + '=' + config.aria2c_opts[key]);
 }
 
-//Start the bugger
+//Start aria2c
 //TODO: Enhance this with checks for already running instances. Exit if launch fails.
 console.log('Starting Aria 2 in daemon mode...');
 var aria2c = cp.spawn(config.berserker_opts.aria2c_executable, args, {detached: true, stdio: 'ignore'});
@@ -20,31 +18,5 @@ aria2c.unref();
 //Connect with aria2c over JSONRPC
 conn.connect(config);
 
-//Start web server
-var server = restify.createServer();
-var io = socketio.listen(server);
-
-server.get('/', function indexHTML(req, res, next) {
-    fs.readFile(__dirname + '/../ng/app/index.html', function(err, data) {
-        if (err) {
-            next(err);
-            return;
-        }
-
-        res.setHeader('Content-Type', 'text/html');
-        res.writeHead(200);
-        res.end(data);
-        next();
-    });
-});
-
-io.sockets.on('connection', function(socket) {
-    socket.emit('news', {hello: 'world'});
-    socket.on('my other event', function(data) {
-        console.log(data);
-    });
-});
-
-server.listen(config.berserker_opts.server_port, function() {
-    console.log('socket.io server listening at %s', server.url);
-});
+//Launch the server
+require('./server')(config, conn);
---
+++ node/server.js
@@ -0,0 +1,60 @@
+//Required modules
+var restify = require('restify'),
+//        socketio = require('socket.io'),
+        fs = require('fs'),
+        mime = require('mime');
+
+
+function init(config, conn) {
+    //Start web server
+    var server = restify.createServer();
+    //var io = socketio.listen(server);
+
+    //io.sockets.on('connection', function(socket) {
+    //    socket.emit('news', {hello: 'world'});
+    //    socket.on('my other event', function(data) {
+    //        console.log(data);
+    //    });
+    //});
+
+    server.listen(config.berserker_opts.server_port, function() {
+        console.log('socket.io server listening at %s', server.url);
+    });
+
+    //Routes
+    server.get('/command/:cmd', function command(req, res, next) {
+        var goptions = {
+            method: req.params.cmd
+        };
+        conn.send(goptions, function(result) {
+            if (result.err) {
+                next(err);
+                return;
+            }
+            res.setHeader('Content-Type', 'application/json');
+            res.writeHead(200);
+            res.end(JSON.stringify(result.obj));
+            next();
+        });
+    });
+
+    server.get(/(.*)/, function sendFile(req, res, next) {
+//        console.dir(req.params);
+        var filename = (req.params[0] === '/') ? __dirname + '/../ng/app/index.html' : __dirname + '/../ng/app/'
+                + req.params[0];
+        fs.readFile(filename, function(err, data) {
+            if (err) {
+                next(err);
+                return;
+            }
+            var mimetype = mime.lookup(filename);
+            res.setHeader('Content-Type', mimetype);
+            res.writeHead(200);
+            res.end(data);
+            next();
+        });
+    });
+}
+
+// Functions which will be available to external callers
+module.exports = init;
