2db0c16484fa66e500873fae980e33f71d4b553b
Aditya Mukhopadhyay <aditya.mukhopadhyay@gmail.com>
2013-05-13 18:23:47 +0530

    Split non-app modules into separate file.

--- ng/app/js/app.js
+++ ng/app/js/app.js
@@ -3,7 +3,7 @@
 /* Modules */
 // Declare app level module which depends on filters, and services
 angular.module('Berserker', ['Berserker.filters', 'Berserker.services', 'Berserker.directives',
-    '$strap.directives', 'compile', 'ui.filters'])
+    '$strap.directives', 'ui.filters'])
         .config(
         [
             '$routeProvider',
@@ -72,72 +72,6 @@ angular.module('Berserker.filters', []).filter('interpolate', ['version', functi
 
 /* Services */
 angular.module('Berserker.services', []).value('version', '0.0.1');
-
-/* Other modules */
-angular.module('compile', [], function($compileProvider) {
-    // configure new 'compile' directive by passing a directive
-    // factory function. The factory function injects the '$compile'
-    $compileProvider.directive('compile', function($compile) {
-        // directive factory creates a link function
-        return function(scope, element, attrs) {
-            scope.$watch(
-                    function(scope) {
-                        // watch the 'compile' expression for changes
-                        return scope.$eval(attrs.compile);
-                    },
-                    function(value) {
-                        // when the 'compile' expression changes
-                        // assign it into the current DOM
-                        element.html(value);
-
-                        // compile the new DOM and link it to the current
-                        // scope.
-                        // NOTE: we only compile .childNodes so that
-                        // we don't get into infinite loop compiling ourselves
-                        $compile(element.contents())(scope);
-                    }
-            );
-        };
-    });
-});
-
-//Source: https://github.com/angular-ui/angular-ui
-angular.module('ui.filters', []).filter('unique', function() {
-    return function(items, filterOn) {
-        if (filterOn === false) {
-            return items;
-        }
-
-        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
-            var hashCheck = {}, newItems = [];
-
-            var extractValueToCompare = function(item) {
-                if (angular.isObject(item) && angular.isString(filterOn)) {
-                    return item[filterOn];
-                } else {
-                    return item;
-                }
-            };
-
-            angular.forEach(items, function(item) {
-                var valueToCheck, isDuplicate = false;
-
-                for (var i = 0; i < newItems.length; i++) {
-                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
-                        isDuplicate = true;
-                        break;
-                    }
-                }
-                if (!isDuplicate) {
-                    newItems.push(item);
-                }
-
-            });
-            items = newItems;
-        }
-        return items;
-    };
-});
 
 /* Helper Functions */
 function sendCommand($scope, $http, command, data, defaultErrHandler) {
---
+++ ng/app/js/modules.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/* Other modules */
+angular.module('compile', [], function($compileProvider) {
+    // configure new 'compile' directive by passing a directive
+    // factory function. The factory function injects the '$compile'
+    $compileProvider.directive('compile', function($compile) {
+        // directive factory creates a link function
+        return function(scope, element, attrs) {
+            scope.$watch(
+                    function(scope) {
+                        // watch the 'compile' expression for changes
+                        return scope.$eval(attrs.compile);
+                    },
+                    function(value) {
+                        // when the 'compile' expression changes
+                        // assign it into the current DOM
+                        element.html(value);
+
+                        // compile the new DOM and link it to the current
+                        // scope.
+                        // NOTE: we only compile .childNodes so that
+                        // we don't get into infinite loop compiling ourselves
+                        $compile(element.contents())(scope);
+                    }
+            );
+        };
+    });
+});
+
+//Source: https://github.com/angular-ui/angular-ui
+angular.module('ui.filters', []).filter('unique', function() {
+    return function(items, filterOn) {
+        if (filterOn === false) {
+            return items;
+        }
+
+        if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+            var hashCheck = {}, newItems = [];
+
+            var extractValueToCompare = function(item) {
+                if (angular.isObject(item) && angular.isString(filterOn)) {
+                    return item[filterOn];
+                } else {
+                    return item;
+                }
+            };
+
+            angular.forEach(items, function(item) {
+                var valueToCheck, isDuplicate = false;
+
+                for (var i = 0; i < newItems.length; i++) {
+                    if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+                        isDuplicate = true;
+                        break;
+                    }
+                }
+                if (!isDuplicate) {
+                    newItems.push(item);
+                }
+
+            });
+            items = newItems;
+        }
+        return items;
+    };
+});
