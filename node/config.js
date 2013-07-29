b012ce8b7711dbcace070748db1efa5e6d7932cc
Aditya Mukhopadhyay <aditya.mukhopadhyay@gmail.com>
2013-07-29 08:15:20 +0530

    Save settings in user's home folder.

--- node/config.js
+++ node/config.js
@@ -3,6 +3,22 @@ var util = require('./util'),
         mkdirp = require('mkdirp'),
         fs = require('fs');
 
+//Init config file in user's home folder.
+var path = process.env.HOME + '/.berserker';
+try {
+    mkdirp.sync(path, 0700);
+    var settingsFile = path + '/settings.json';
+    if (fs.existsSync(settingsFile)) {
+        settings = require(settingsFile);
+    }
+    else {
+        saveSettings();
+    }
+}
+catch (err) {
+    console.warn('WARNING: Cannot create folder "%s". Settings changes will not persist across runs.', path);
+}
+
 //Replace {ENV} placeholders with their actual values.
 util.traverse(settings, function(obj, key, value) {
     if (typeof value === 'string') {
@@ -21,25 +37,25 @@ for (var i = 0; i < dirs.length; ++i) {
 }
 dirs.push(settings.aria2c.dir);
 for (var i = 0; i < dirs.length; ++i) {
-    mkdirp(dirs[i], 0700, function(err) {
-        if (err) {
-            console.warn('WARNING: Cannot create folder "%s". Aria may fail to start properly.', err.path);
-        }
-    });
+    try {
+        mkdirp.sync(dirs[i], 0700);
+    }
+    catch (err) {
+        console.error('ERROR: Cannot create folder "%s".', dirs[i]);
+        process.exit(1);
+    }
 }
 
 var files = [settings.aria2c['save-session'], settings.aria2c['input-file'], settings.aria2c.log];
 for (var i = 0; i < files.length; ++i) {
-    fs.open(files[i], 'a', function(err, fd) {
-        if (err) {
-            console.warn(
-                    'WARNING: The file "%s" cannot be opened for writing. Aria may fail to start properly.',
-                    err.path);
-        }
-        else {
-            fs.close(fd);
-        }
-    });
+    try {
+        var fd = fs.openSync(files[i], 'a');
+        fs.close(fd);
+    }
+    catch (err) {
+        console.error('ERROR: The file "%s" cannot be opened for writing.', files[i]);
+        process.exit(1);
+    }
 }
 
 //Register listener for config save.
@@ -48,15 +64,22 @@ util.eventEmitter.on('aria2.changeGlobalOption', function(options) {
         for (var key in options[0]) {
             settings.aria2c[key] = options[0][key];
         }
-        fs.writeFile(__dirname + '/settings.json', JSON.stringify(settings, null, 4), function(err) {
-            if (err) {
-                console.error('ERROR: Cannot write to settings file "%s"', err.path);
-            }
-            else {
-                console.log("INFO: Settings saved.");
-            }
-        });
+        saveSettings();
     }
 });
+
+function saveSettings(path) {
+    path = path || process.env.HOME + '/.berserker/settings.json';
+    fs.writeFile(path, JSON.stringify(settings, null, 4),
+            function(err) {
+                if (err) {
+                    console.error('ERROR: Cannot write to settings file "%s"',
+                            err.path);
+                }
+                else {
+                    console.log("INFO: Settings saved.");
+                }
+            });
+}
 
 module.exports = settings;
